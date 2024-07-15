c5ef064b5d946c37780d440d4aa3d5585c96c843
kittimas <kittimasdev@gmail.com>
2024-07-15 11:49:39 +0700

    s

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
-import  Gift from "@/components/Gift";
+import { Gift } from "@/components/Gift";
 
 export default function Home() {
   return (
-      <Gift />
+    <Gift />
   );
 }
